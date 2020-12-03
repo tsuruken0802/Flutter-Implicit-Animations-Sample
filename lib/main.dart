94057168a9da6da6aecf4e80b58b6f52056854c6
鶴本賢太朗 <tsurumotokentarou@gmail.com>
2020-12-03 13:36:26 +0900

    create animation sample ListView

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,73 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AnimatedContainerScreen(),
+      home: AnimationSampleList(),
+    );
+  }
+}
+
+class AnimationSampleList extends StatelessWidget {
+  List<Widget> _list(BuildContext context) {
+    return [
+      AnimationSampleListItem(
+          itemName: 'AnimatedContainer',
+          onPressItem: () {
+            this._navigator(context, AnimatedContainerScreen());
+          }),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> list = _list(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Implicit Animations Sample'),
+      ),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return list[index];
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider(color: Colors.black);
+          },
+          itemCount: list.length),
+    );
+  }
+
+  void _navigator(BuildContext context, Widget nextScreen) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return nextScreen;
+    }));
+  }
+}
+
+class AnimationSampleListItem extends StatelessWidget {
+  final String itemName;
+  final Function onPressItem;
+
+  AnimationSampleListItem(
+      {@required this.itemName, @required this.onPressItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Text(
+          this.itemName,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      onTap: this.onPressItem,
     );
   }
 }
